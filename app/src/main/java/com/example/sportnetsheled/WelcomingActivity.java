2a039c126657343cf1c4d8ab92f5f1e6543d5e66
bencohenprojects@gmail.com <ben482248220>
2022-02-01 18:28:12 +0200

    lab home

---
+++ app/src/main/java/com/example/sportnetsheled/WelcomingActivity.java
@@ -0,0 +1,14 @@
+package com.example.sportnetsheled;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcoming);
+    }
+}
