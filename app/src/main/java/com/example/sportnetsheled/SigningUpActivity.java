6908c861fc14983caa230e4642dacd7dafd01428
bencohenprojects@gmail.com <ben482248220>
2022-02-02 08:52:04 +0200

    lab 2.2 08:51

---
+++ app/src/main/java/com/example/sportnetsheled/SigningUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.sportnetsheled;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigningUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signing_up);
+    }
+}
