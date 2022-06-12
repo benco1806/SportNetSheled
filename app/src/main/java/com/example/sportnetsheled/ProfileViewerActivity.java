f29e82183cde15eb7cf8197c4186d9edea7b97bd
benco1806 <bencohenprojects@gmail.com>
2022-06-13 00:54:50 +0300

    רק לתקן עד ההצגה הסופית שלא יהיו פדיחות

---
+++ app/src/main/java/com/example/sportnetsheled/ProfileViewerActivity.java
@@ -0,0 +1,13 @@
+package com.example.sportnetsheled;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileViewerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile);
+    }
+}
